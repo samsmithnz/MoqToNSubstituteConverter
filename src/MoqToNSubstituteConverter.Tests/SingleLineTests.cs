603f2a475805365cb3655aa58883b5df62b4f243
Sam Smith <samsmithnz@gmail.com>
2023-08-11 05:41:54 -0400

    Added more failing tests

---
+++ src/MoqToNSubstituteConverter.Tests/SingleLineTests.cs
@@ -0,0 +1,87 @@
+﻿namespace MoqToNSubstituteConverter.Tests
+{
+    [TestClass]
+    public class SingleLineTests
+    {
+        [TestMethod]
+        public void UsingTest()
+        {
+            //Arrange
+            Conversion conversion = new();
+            string code = @"
+using Moq;
+";
+
+            //Act
+            ConversionResponse gitHubOutput = conversion.ConvertMoqToNSubstitute(code);
+
+            //Assert
+            string expected = @"
+using NSubstitute;
+";
+
+            Assert.AreEqual(expected, gitHubOutput.ConvertedCode);
+        }
+
+        [TestMethod]
+        public void InitializeVariableTest()
+        {
+            //Arrange
+            Conversion conversion = new();
+            string code = @"
+Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+";
+
+            //Act
+            ConversionResponse gitHubOutput = conversion.ConvertMoqToNSubstitute(code);
+
+            //Assert
+            string expected = @"
+IConfiguration mockConfiguration = new Substitute.For<IConfiguration>();
+";
+
+            Assert.AreEqual(expected, gitHubOutput.ConvertedCode);
+        }
+
+        [TestMethod]
+        public void ObjectTest()
+        {
+            //Arrange
+            Conversion conversion = new();
+            string code = @"
+MyStorageTable context = new MyStorageTable(mockConfiguration.Object);
+";
+
+            //Act
+            ConversionResponse gitHubOutput = conversion.ConvertMoqToNSubstitute(code);
+
+            //Assert
+            string expected = @"
+MyStorageTable context = new MyStorageTable(mockConfiguration);
+";
+
+            Assert.AreEqual(expected, gitHubOutput.ConvertedCode);
+        }
+
+        [TestMethod]
+        public void SetupTest()
+        {
+            //Arrange
+            Conversion conversion = new();
+            string code = @"
+mock.Setup(repo => repo.CheckResult(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(true));
+";
+
+            //Act
+            ConversionResponse gitHubOutput = conversion.ConvertMoqToNSubstitute(code);
+
+            //Assert
+            string expected = @"
+mock.CheckResult(Arg.Any<string>(), Arg.Any<string>()).Returns(Task.FromResult(true));
+";
+
+            Assert.AreEqual(expected, gitHubOutput.ConvertedCode);
+        }
+       
+    }
+}
