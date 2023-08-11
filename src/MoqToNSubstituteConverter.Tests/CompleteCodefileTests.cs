35f47fa073e5f206dc6edb81486fd518d7408da1
Sam Smith <samsmithnz@gmail.com>
2023-08-11 05:34:36 -0400

    Created a complete unit test that fails.

--- src/MoqToNSubstituteConverter.Tests/CompleteCodefileTests.cs
+++ src/MoqToNSubstituteConverter.Tests/CompleteCodefileTests.cs
@@ -4,12 +4,46 @@ namespace MoqToNSubstituteConverter.Tests;
 public class CompleteCodefileTests
 {
     [TestMethod]
-    public void UsingTest()
+    public void CompleteUnitTestFileTest()
     {
         //Arrange
         Conversion conversion = new Conversion();
         string code = @"
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Tests
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    [TestClass]
+    public class MyUnitTests
+    {
+
+        [TestMethod]
+        public async Task CheckMyUnitTest()
+        {
+            //Arrange
+            Mock<IConfiguration> mockConfiguration = new Mock<IConfiguration>();
+            MyStorageTable context = new MyStorageTable(mockConfiguration.Object);
+            Mock<IMyStorageTable> mock = new Mock<IMyStorageTable>();
+            mock.Setup(repo => repo.CheckResult(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(true));
+            MyController controller = new MyController(mock.Object);
+            string name = ""abc"";
+            string environment = ""def"";
+
+            //Act
+            bool result = await controller.CheckResult(name, environment);
+
+            //Assert
+            Assert.IsTrue(result == true);
+        }
+    }
+}
 ";
 
         //Act
@@ -17,7 +51,41 @@ using Moq;
 
         //Assert
         string expected = @"
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Tests
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    [TestClass]
+    public class MyUnitTests
+    {
+
+        [TestMethod]
+        public async Task CheckMyUnitTest()
+        {
+            //Arrange
+            IConfiguration mockConfiguration = Substitute.For<IConfiguration>();
+            MyStorageTable context = new(mockConfiguration);
+            var mock = Substitute.For<IMyStorageTable>();
+            mock.CheckResult(Arg.Any<string>(), Arg.Any<string>()).Returns(Task.FromResult(true));
+            MyController controller = new MyController(mock);
+            string name = ""abc"";
+            string environment = ""def"";
+
+            //Act
+            bool result = await controller.CheckResult(name, environment);
+
+            //Assert
+            Assert.IsTrue(result == true);
+        }
+    }
+}
 ";
 
         Assert.AreEqual(expected, gitHubOutput.ConvertedCode);
