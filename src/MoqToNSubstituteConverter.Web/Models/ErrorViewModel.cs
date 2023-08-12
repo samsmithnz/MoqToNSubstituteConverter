e42a1e41cb1cce8f3e6d97c9cebb71f610d72f08
Sam Smith <samsmithnz@gmail.com>
2023-08-11 21:56:44 -0400

    Added new website + css

---
+++ src/MoqToNSubstituteConverter.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoqToNSubstituteConverter.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
