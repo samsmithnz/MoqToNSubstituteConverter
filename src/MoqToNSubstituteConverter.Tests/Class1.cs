07d95421e63fa6830163f0d226870ef50c9863b4
Sam Smith <samsmithnz@gmail.com>
2023-08-13 09:34:19 -0400

    Code gardening

---
+++ src/MoqToNSubstituteConverter.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace MoqToNSubstituteConverter.Tests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Conversion conversion = new();
+            ConversionResponse gitHubOutput = conversion.
+                ConvertMoqToNSubstitute("");
+        }
+    }
+}
