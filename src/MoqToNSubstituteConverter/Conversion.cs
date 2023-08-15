03177026225e286c094adaa8965aae3f5948aa81
Sam Smith <samsmithnz@gmail.com>
2023-08-15 07:48:56 -0400

    Reenabled callback processing

--- src/MoqToNSubstituteConverter/Conversion.cs
+++ src/MoqToNSubstituteConverter/Conversion.cs
@@ -66,7 +66,7 @@ public class Conversion
         processedLine = ProcessVerify(processedLine);
 
         //process callback
-        //TODO: processedLine = ProcessCallback(processedLine);
+        processedLine = ProcessCallback(processedLine);
 
         //Feed the line back into the final result
         //processedCode.AppendLine(processedLine);
