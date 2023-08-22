c11ebd5065996616a1f7f4f0fda8f50041c6cf41
Sam Smith <samsmithnz@gmail.com>
2023-08-22 07:18:21 -0400

    commenting out samples for now

--- src/MoqToNSubstituteConverter.Tests/NSubstituteSamples.cs
+++ src/MoqToNSubstituteConverter.Tests/NSubstituteSamples.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.Core.Configuration;
-using Moq;
-using NSubstitute;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Castle.Core.Configuration;
+//using Moq;
+//using NSubstitute;
 
-namespace MoqToNSubstituteConverter.Tests
-{
-    [TestClass]
-    public class NSubstituteSamples
-    {
-        [TestMethod]
-        public void NSubstituteTest()
-        {
-            //Arrange
-            IConfiguration mockConfiguration = Substitute.For<IConfiguration>();
-        }
-    }
-}
+//namespace MoqToNSubstituteConverter.Tests
+//{
+//    [TestClass]
+//    public class NSubstituteSamples
+//    {
+//        [TestMethod]
+//        public void NSubstituteTest()
+//        {
+//            //Arrange
+//            IConfiguration mockConfiguration = Substitute.For<IConfiguration>();
+//        }
+//    }
+//}
